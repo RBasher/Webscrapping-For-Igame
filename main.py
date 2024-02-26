2c69622b11ab044cab7de6412324b1c914254ab1
RBasher <113730105+RBasher@users.noreply.github.com>
2024-02-26 06:44:38 -0500

    all games

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ from bs4 import BeautifulSoup
 
 url = "https://apps.apple.com/us/genre/ios-games/id6014"
 html = requests.get(url)
-s = BeautifulSoup(html.content, 'html.parser')
-x = s.find(id="selectedcontent")
-y = x.find_all("a")
-# Create a list to store game names
-game = [["Game Name"]]  # Add a header row
-for i in y:
-    game.append([i.text])
-print(game)
-with open('igame.csv', 'w+', encoding="utf-8", newline='') as file:
-    write = csv.writer(file)
-    for row in game:
-        write.writerow(row)
+gs = BeautifulSoup(html.content, 'html.parser')
+gx = gs.find("ul", {"class": "list top-level-subgenres"})
+gy=gx.find_all("a")
+
+print(gy)
+def xv(url):
+
+    html = requests.get(url)
+    s = BeautifulSoup(html.content, 'html.parser')
+    x = s.find(id="selectedcontent")
+    y = x.find_all("a")
+    # Create a list to store game names
+    game = [["Game Name"]]  # Add a header row
+    for i in y:
+        game.append([i.text])
+    print(game)
+    with open('igame.csv', 'a', encoding="utf-8", newline='') as file:
+        write = csv.writer(file)
+        for row in game:
+            write.writerow(row)
+for genrate_url in gy:
+    genrate_url = genrate_url['href']  # Extract the href attribute from the anchor tag
+    xv(genrate_url)
